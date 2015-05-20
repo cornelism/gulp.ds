1f4cd26d44c7fcf12445647578a4d9cbea86d0f0
Thomas De Beuckelaer <thomasdebeuckelaer@dropsolid.com>
2015-05-20 17:02:43 +0200

    Update normalize

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,17 +3,17 @@ var sass = require('gulp-sass');
 var globbing = require('gulp-css-globbing');
 var sourcemaps = require('gulp-sourcemaps');
 var livereload = require('gulp-livereload');
+var handleErrors = require('../util/handleErrors');
 var config = require('../config').sass;
 
 gulp.task('sass', ['images'], function () {
-  return gulp.src(config.src)
+  gulp.src(config.src)
     .pipe(globbing({
         // Configure it to use SCSS files
         extensions: ['.scss']
     }))
-    .pipe(sass({
-      errLogToConsole: true
-    }))
+    .pipe(sass.sync().on('error', sass.logError))
+
     .pipe(gulp.dest(config.dest))
     .pipe(livereload())
     .pipe(sourcemaps.init())
